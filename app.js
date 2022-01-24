0abb2a4fbc05ae45e2e76e90ed266fce95ee0de6
ShamGawd31 <97768084+ShamGawd31@users.noreply.github.com>
2022-01-23 23:10:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// Variables store information
+
+// static variables
+// let name = "Shamar";
+// let name = "Johnson";
+
+//user input variables
+//let name = prompt(message:"What is your name?");
+//let lname = prompt(message: "What is your last name?");
+
+
+
+
+//let fname = name+" "+lname;
+
+//variables for numbers;
+
+let number1 = Number(prompt("What Is your subtotal?"));
+//let number2 = Number(prompt("second number"));
+
+let sum = number1*.07;
+let map = number1+sum
+let tar = map*.05;
+let totalPrice = map+tar;
+
+//let totalPrice = (number1+number2)-((number1+number2)*0.3);
+
+// write value of variables to the web page
+
+// writing for the strings
+
+// document.body.innerHTML+fName;
+
+//writing for numbers
+
+document.body.innerHTML=totalPrice;
